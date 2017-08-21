6d1dce5450953ec2b9bf7a22b1104a857432a160
Daniel Lemper <ich@dlemper.de>
2017-08-21 22:49:08 +0200

    changed code base to es2015 and changed data emiting

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 /*
 # Respsonse Format
 
@@ -16,101 +14,79 @@ Disconnected:
 Date;DISCONNECT;ConnectionID;DurationInSeconds;
 */
 
-var net    = require('net');
-var events = require('events');
+const net = require('net');
+const events = require('events');
 
-var CallMonitor = function (host, port) {
-  var self = this;
-  this.call = {};
+class CallMonitor {
+  constructor(host, port) {
+    this.call = {};
 
-  port = port || 1012;
+    port = port || 1012;
 
-  function fritzboxDateToUnix(string) {
-    var d = string.match(/[0-9]{2}/g);
-    var result = '';
-    result += '20' + d[2] + '-' + d[1] + '-' + d[0];
-    result += ' ' + d[3] + ':' + d[4] + ':' + d[5];
-    return Math.floor(new Date(result).getTime() / 1000);
+    const client = net.createConnection(port, host);
+    client.setKeepAlive(true, 300 * 1000);
+
+    client.addListener('error', function (error) {
+      this.emit('error', error);
+    });
+
+    client.addListener('data', function (chunk) {
+      const data = this.parseMessage(chunk);
+
+      this.emit('event', data);
+
+      if (data.type === 'ring') {
+        this.emit('incoming', data);
+      } else if (data.type === 'call') {
+        this.emit('outgoing', data);
+      } else if (data.type === 'connect') {
+        this.emit('connected', data);
+      } else if (data.type === 'disconnect') {
+        this.emit('disconnected', data);
+      }
+    });
+
+    client.addListener('end', function () {
+      client.end();
+    });
   }
 
-  function parseMessage(buffer) {
-    var message = buffer.toString()
+  static fritzboxDateToUnix(string) {
+    const d = string.match(/[0-9]{2}/g);
+
+    return new Date(`20${d[2]}`, d[1], d[0], d[3], d[4], d[5]);
+  }
+
+  static parseMessage(buffer) {
+    const message = buffer.toString()
                   .toLowerCase()
                   .replace(/[\n\r]$/, '')
                   .replace(/;$/, '')
                   .split(';');
-    message[0] = fritzboxDateToUnix(message[0]);
-    return message;
+
+    const data = {
+      type: message[1],
+      connectionId: message[2],
+      date: fritzboxDateToUnix(message[0]),
+    }
+
+    if (data.type === 'ring') {
+      data.caller = message[3];
+      data.called = message[4];
+    } else if (data.type === 'call') {
+      data.extension = message[3];
+      data.caller = message[4];
+      data.called = message[5];
+    } else if (data.type === 'connect') {
+      data.extension = message[3];
+      data.number = message[4];
+    } else if (data.type === 'disconnect') {
+      data.duration = message[3];
+    }
+
+    return data;
   }
-
-  var client = net.createConnection(port, host);
-
-  client.addListener('error', function (error) {
-    self.emit('error', error);
-  });
-
-  client.addListener('data', function (chunk) {
-    var data = parseMessage(chunk);
-
-    if (data[1] === 'ring') {
-      self.call[data[2]] = {
-        type: 'inbound',
-        start: data[0],
-        caller: data[3],
-        called: data[4]
-      };
-      self.emit('inbound', {
-        time: data[0],
-        caller: data[3],
-        called: data[4]
-      });
-      return;
-    }
-
-    if (data[1] === 'call') {
-      self.call[data[2]] = {
-        type: 'outbound',
-        start: data[0],
-        extension: data[3],
-        caller: data[4],
-        called: data[5]
-      };
-      self.emit('outbound', {
-        time: data[0],
-        extension: data[3],
-        caller: data[4],
-        called: data[5]
-      });
-      return;
-    }
-
-    if (data[1] === 'connect') {
-      self.call[data[2]]['connect'] = data[0];
-      self.emit('connected', {
-        time: data[0],
-        extension: self.call[data[2]]['extension'],
-        caller: self.call[data[2]]['caller'],
-        called: self.call[data[2]]['called']
-      });
-      return;
-    }
-
-    if (data[1] === 'disconnect') {
-      self.call[data[2]].disconnect = data[0];
-      self.call[data[2]].duration   = parseInt(data[3], 10);
-
-      var call = self.call[data[2]];
-      delete(self.call[data[2]]);
-      self.emit('disconnected', call);
-      return;
-    }
-
-  });
-
-  client.addListener('end', function () {
-    client.end();
-  });
-};
+}
 
 CallMonitor.prototype = new events.EventEmitter();
 
